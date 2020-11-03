3b83007824a3cdcb709deef1994825659bead6f0
Jimi <jimijenneskens@gmail.com>
2020-11-03 14:03:09 -0600

    changesnew

---
+++ app.js
@@ -0,0 +1,10 @@
+// append ids to the dropdown 
+  
+d3.json('data/data.json').then((data)=>{
+    var id=data.names;
+    console.log(data.metadata);
+    var select=d3.selectAll('#selDataset');
+    Object.entries(id).forEach(([i,v])=>{
+        select.append('option').text(v);
+    })
+})
